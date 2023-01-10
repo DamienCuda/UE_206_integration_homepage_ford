3f4f0b8605ec93d30dff46c0700b8515503e1034
Damien Cuda <contact@damiencuda.com>
2023-01-10 10:53:45 +0100

    add link new css

--- php-modules/header.php
+++ php-modules/header.php
@@ -5,12 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/configurateur.css">
     <script src="assets/js/script.js" defer></script>
     <title>Ford | <?=$title?></title>
 </head>
     <header>
         <nav id="top_nav" class="inline_padding bold">
-            <ul class="flex_container">
+            <ul class="flex_container wrap">
                 <li><a  href="#">Offres</a></li>
                 <li><a  href="#">Entreprises</a></li>
                 <li><a  href="#">Utilitaires</a></li>
@@ -23,7 +25,7 @@
                 <a href="index.php"><img src="assets/img/logo.png" alt="logo Ford"></a> 
             </div>
             <nav>
-                <ul class="flex_container">
+                <ul class="flex_container wrap">
                     <li><a  href="#">Véhicules</a></li>
                     <li><a  href="#">Achat</a></li>
                     <li><a  href="#">Financement</a></li>
