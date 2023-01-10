1f54349442c526a1afc1211ddefebf3386990ed6
Damien Cuda <contact@damiencuda.com>
2023-01-10 09:22:12 +0100

    add title to footer

--- php-modules/footer.php
+++ php-modules/footer.php
@@ -2,24 +2,24 @@
         <div class="footer_list_container">
             <h4 class="footer_list_title bold">Véhicules</h4>
             <ul class="footer_list">
-                <li><a href="#">Véhicules Particuliers</a></li>
-                <li><a href="#">Utilitaires</a></li>
-                <li><a href="#">Citadines</a></li>
-                <li><a href="#">Hybride et Électrique</a></li>
-                <li><a href="#">Familliales et Ludospaces</a></li>
-                <li><a href="#">Monospace</a></li>
-                <li><a href="#">Performance</a></li>
-                <li><a href="#">SUV</a></li>
+                <li><a href="#" title="Véhicules Particuliers">Véhicules Particuliers</a></li>
+                <li><a href="#" title="Utilitaires">Utilitaires</a></li>
+                <li><a href="#" title="Citadines">Citadines</a></li>
+                <li><a href="#" title="Hybride et Électrique">Hybride et Électrique</a></li>
+                <li><a href="#" title="Familliales et Ludospaces">Familliales et Ludospaces</a></li>
+                <li><a href="#" title="Monospace">Monospace</a></li>
+                <li><a href="#" title="Performance">Performance</a></li>
+                <li><a href="#" title="SUV">SUV</a></li>
             </ul>
         </div>
 
         <div class="footer_list_container">
             <h4 class="footer_list_title bold">Découvrez Ford</h4>
             <ul class="footer_list">
-                <li><a href="#">À propos de Ford</a></li>
-                <li><a href="#">L'actualité Ford</a></li>
-                <li><a href="#">Nous rejoindre</a></li>
-                <li><a href="#">Développement durable</a></li>
+                <li><a href="#" title="À propos de Ford">À propos de Ford</a></li>
+                <li><a href="#" title="L'actualité Ford">L'actualité Ford</a></li>
+                <li><a href="#" title="Nous rejoindre">Nous rejoindre</a></li>
+                <li><a href="#" title="Développement durable">Développement durable</a></li>
             </ul>
         </div>
 
@@ -27,19 +27,17 @@
             <h4 class="footer_list_title bold">Véhicules</h4>
             <div class="footer_list grid_container">
                 <ul>
-                    <li><a href="#">EcoSport</a></li>
-                    <li><a href="#">Focus</a></li>
-                    <li><a href="#">Mustang Mach-E</a></li>
-                    <li><a href="#">Mustang</a></li>
-                    <li><a href="#">Focus ST</a></li>
-                    <li><a href="#">Tournéo Connect</a></li>
-                </ul>
-                <ul>
-                    <li><a href="#">Puma</a></li>
-                    <li><a href="#">Kuga</a></li>
-                    <li><a href="#">Explorer</a></li>
-                    <li><a href="#">Puma ST</a></li>
-                    <li><a href="#">Ford GT</a></li>
+                    <li><a href="#" title="EcoSport">EcoSport</a></li>
+                    <li><a href="#" title="Focus">Focus</a></li>
+                    <li><a href="#" title="Mustang Mach-E">Mustang Mach-E</a></li>
+                    <li><a href="#" title="Mustang">Mustang</a></li>
+                    <li><a href="#" title="Focus ST">Focus ST</a></li>
+                    <li><a href="#" title="Tournéo Connect">Tournéo Connect</a></li>
+                    <li><a href="#" title="Puma">Puma</a></li>
+                    <li><a href="#" title="Kuga">Kuga</a></li>
+                    <li><a href="#" title="Explorer">Explorer</a></li>
+                    <li><a href="#" title="Puma ST">Puma ST</a></li>
+                    <li><a href="#" title="Ford GT">Ford GT</a></li>
                 </ul>
             </div>
         </div>
