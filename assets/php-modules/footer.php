2a041cb0cfaef4f64a473410de0dca649ea6f8f4
Damien Cuda <contact@damiencuda.com>
2023-01-10 12:46:48 +0100

    fix footer

---
+++ assets/php-modules/footer.php
@@ -0,0 +1,44 @@
+<footer id="main_footer" class="grid_container inline_padding">
+    <div class="footer_list_container">
+        <h4 class="footer_list_title bold">Véhicules</h4>
+        <ul class="footer_list">
+            <li><a href="#" title="Véhicules Particuliers">Véhicules Particuliers</a></li>
+            <li><a href="#" title="Utilitaires">Utilitaires</a></li>
+            <li><a href="#" title="Citadines">Citadines</a></li>
+            <li><a href="#" title="Hybride et Électrique">Hybride et Électrique</a></li>
+            <li><a href="#" title="Familliales et Ludospaces">Familliales et Ludospaces</a></li>
+            <li><a href="#" title="Monospace">Monospace</a></li>
+            <li><a href="#" title="Performance">Performance</a></li>
+            <li><a href="#" title="SUV">SUV</a></li>
+        </ul>
+    </div>
+
+    <div class="footer_list_container">
+        <h4 class="footer_list_title bold">Découvrez Ford</h4>
+        <ul class="footer_list">
+            <li><a href="#" title="À propos de Ford">À propos de Ford</a></li>
+            <li><a href="#" title="L'actualité Ford">L'actualité Ford</a></li>
+            <li><a href="#" title="Nous rejoindre">Nous rejoindre</a></li>
+            <li><a href="#" title="Développement durable">Développement durable</a></li>
+        </ul>
+    </div>
+
+    <div class="footer_list_container">
+        <h4 class="footer_list_title bold">Véhicules</h4>
+        <div class="footer_list">
+            <ul class="flex_container wrap">
+                <li><a href="#" title="EcoSport">EcoSport</a></li>
+                <li><a href="#" title="Focus">Focus</a></li>
+                <li><a href="#" title="Mustang Mach-E">Mustang Mach-E</a></li>
+                <li><a href="#" title="Mustang">Mustang</a></li>
+                <li><a href="#" title="Focus ST">Focus ST</a></li>
+                <li><a href="#" title="Tournéo Connect">Tournéo Connect</a></li>
+                <li><a href="#" title="Puma">Puma</a></li>
+                <li><a href="#" title="Kuga">Kuga</a></li>
+                <li><a href="#" title="Explorer">Explorer</a></li>
+                <li><a href="#" title="Puma ST">Puma ST</a></li>
+                <li><a href="#" title="Ford GT">Ford GT</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
