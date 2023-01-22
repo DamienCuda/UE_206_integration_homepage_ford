3f3eaf595d2e821ecb111faa6c4fca35b7628835
Damien Cuda <contact@damiencuda.com>
2023-01-22 04:31:20 +0100

    commentaires JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,17 +12,17 @@ $(document).ready(function() {
     // Selection du type
     $type_input.change(function(){
         $vehicules.each(function(key, value){
-            $(value).hide();
+            $(value).hide(); //On cache toutes les voitures
         })
         $selected_type = $(this).find(":selected").val();
         if($selected_type == ""){
             $vehicules.each(function(key, value){
-                $(value).show();
+                $(value).show(); //On gère l'erreur
             })
         }else{
             $vehicules.each(function(key, value){
                 if($(value).data('type') == $selected_type){
-                    $(value).show();
+                    $(value).show(); //On ne montre que celle qui corresponde à la selection
                 }
             })
         }
@@ -30,39 +30,38 @@ $(document).ready(function() {
 
     // Selection du prix
     $budget_input.change(function(){
-        $erreur.hide();
+        $erreur.hide(); //On cache le message d'erreur en cas de clique multiple
         $vehicules.each(function(key, value){
-            $(value).hide();
+            $(value).hide(); //On cache toutes les voitures
         })
         $selected_price = $(this).val();
         $i = 0
         $vehicules.each(function(key, value){
             if($(value).data('prix') <= $selected_price){
-                $i++;
-                $(value).show();
+                $i++; //On incrémente si une voiture correspond
+                $(value).show(); //On ne montre que celle qui corresponde à la selection
             }
         })
         if($i == 0){
-            $erreur.show();
-            
+            $erreur.show(); //On gère l'erreur
         }
     })
     
     // Selection de la puissance
     $power_input.change(function(){
-        $erreur.hide();
+        $erreur.hide(); //On cache le message d'erreur en cas de clique multiple
         $vehicules.each(function(key, value){
-            $(value).hide();
+            $(value).hide(); //On cache toutes les voitures
         })
         $selected_power = $(this).find(":selected").val();
-        if($selected_power == ""){
+        if($selected_power == ""){ //On gère l'erreur
             $vehicules.each(function(key, value){
-                $(value).show();
+                $(value).show(); //On ne montre que celle qui corresponde à la selection
             })
         }else{
             $vehicules.each(function(key, value){
                 if($(value).data('power') == $selected_power){
-                    $(value).show();
+                    $(value).show(); //On ne montre que celle qui corresponde à la selection
                 }
             })
         }
@@ -70,21 +69,20 @@ $(document).ready(function() {
 
     // Selection de la couleur
     $color_input.change(function(){
-        $erreur.hide();
+        $erreur.hide(); //On cache le message d'erreur en cas de clique multiple
         $vehicules.each(function(key, value){
-            $(value).hide();
+            $(value).hide(); //On cache toutes les voitures
         })
         $checked_color = $(this).data('color');
         $i = 0
         $vehicules.each(function(key, value){
             if($(value).data('color') == $checked_color){
-                $i++;
+                $i++; //On incrémente si une voiture correspond
                 $(value).show();
             }
         })
-        if($i == 0){
-            $erreur.show();
-            
+        if($i == 0){ 
+            $erreur.show();  //On gère l'erreur
         }
     })
 });
